4c30105b9c2e288d8a6d6e0c81a4e96d9de9572b
Homayoon Sadeghi <homayoon.9171@gmail.com>
2021-05-23 17:51:41 +0430

    Fix Flask-API Configs

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 
 from flask import Flask, jsonify, request
-from Summarize import summarize
+from flask_cors import CORS
+from summarize import summarize
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/predict/*": {"origins": "*"}})
+
+if app.config['ENV'] == 'production':
+    app.config.from_object('config.ProductionConfig')
+elif app.config['ENV'] == 'development':
+    app.config.from_object('config.DevelopmentConfig')
+else:
+    app.config.from_object('config.TestingConfig')
 
 
 @app.route('/predict', methods=['POST'])
@@ -13,6 +22,5 @@ def get_summarize():
     data = request.form['text']
     return jsonify({'result': summarize(str(data))})
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8000)
---
+++ summarize.py
@@ -0,0 +1,42 @@
+from transformers import (
+    T5TokenizerFast as T5Tokenizer
+)
+load = False
+
+
+def init():
+    global load
+    if not load:
+        global torch, model, tokenizer
+        torch = __import__("torch")
+        model = torch.load('./model-store/model.pth')
+        tokenizer = T5Tokenizer.from_pretrained('./config/')
+        load = True
+
+
+def summarize(text: str):
+    init()
+    text_encoding = tokenizer(
+        text,
+        max_length=512,
+        padding="max_length",
+        truncation=True,
+        return_attention_mask=True,
+        add_special_tokens=True,
+        return_tensors="pt"
+    )
+    generated_ids = model.generate(
+        input_ids=text_encoding["input_ids"],
+        attention_mask=text_encoding["attention_mask"],
+        max_length=150,
+        num_beams=2,
+        repetition_penalty=2.5,
+        length_penalty=1.0,
+        early_stopping=True
+    )
+    preds = [
+        tokenizer.decode(gen_id, skip_special_tokens=True,
+                         clean_up_tokenization_spaces=True)
+        for gen_id in generated_ids
+    ]
+    return "".join(preds)

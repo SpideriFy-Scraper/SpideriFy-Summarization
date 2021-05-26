594203515518a4f87e81fba940a49542112b4d58
Homayoonn <homayoon.9171@gmail.com>
2021-05-26 20:06:39 +0430

    Add Cors For FastAPI

--- api.py
+++ api.py
@@ -1,10 +1,24 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from summarize import get_model, Model
 
+app = FastAPI(title="Statelss Summarization MicroService")
 
-app = FastAPI()
+origins = [
+    "http://spiderify-api:8080",
+    "http://spiderify-api",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class SummarizationRequest(BaseModel):
     text: str
